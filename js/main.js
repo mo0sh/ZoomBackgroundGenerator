4f9a634c4598ae132eb5523a5207d09325c83d84
David DeMello <daviddemello@gmail.com>
2020-12-22 18:10:52 -0500

    Adds support for different pre-logo background images

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let canvas, canvashd, ctxorig, ctxhd, $createimage, $optionHD, $option43, $imgs,
-    $first, $last, $pronouns, $radialParm, $optionSimple, $option75;
+    $first, $last, $pronouns, $radialParm, $optionSimple, $option75, $optionGradient, $optionTextured;
 
 // Initialize variable references and load assets
 function init () {
@@ -16,17 +16,26 @@ function init () {
   $optionSimple = document.getElementById("optionSimple");
   $option75 = document.getElementById("option75");
 
+  $optionGradient = document.getElementById("optionGradient");
+  $optionTextured = document.getElementById("optionTextured");
+
   $imgs = document.getElementById("imgs");
 
   $first =document.getElementById("first");
   $last = document.getElementById("last");
   $pronouns = document.getElementById("pronouns");
 
-  $bgImgHD = document.getElementById("bgImgHD");
-  $bgImg43 = document.getElementById("bgImg43");
-  $bgImgHD75 = document.getElementById("bgImgHD75");
-  $bgImg4375 = document.getElementById("bgImg4375");
-  $activeImg = $bgImgHD;
+  $bgImages = {
+    bgImgHD: document.getElementById("bgImgHD"),
+    bgImg43: document.getElementById("bgImg43"),
+    bgImgHD75: document.getElementById("bgImgHD75"),
+    bgImg4375: document.getElementById("bgImg4375"),
+    grad_bgImgHD: document.getElementById("grad_bgImgHD"),
+    grad_bgImg43: document.getElementById("grad_bgImg43"),
+    grad_bgImgHD75: document.getElementById("grad_bgImgHD75"),
+    grad_bgImg4375: document.getElementById("grad_bgImg4375")
+  };
+  $activeImg = $bgImages['bgImgHD'];
 
   let queryString = window.location.search;
   let urlParams = new URLSearchParams(queryString);
@@ -49,15 +58,22 @@ function init () {
   update();
 }
 
+function setActiveImg() {
+  let key = '';
+  key = ($optionGradient.checked) ? 'grad_' : key;
+  key = ($optionHD.checked) ? key + 'bgImgHD' : key + 'bgImg43';
+  key = ($option75.checked) ? key + '75' : key;
+  $activeImg = $bgImages[key];
+  return true;
+}
+
 // Draw the HD version of the canvas
 function drawHD(){
-  $activeImg = (optionSimple.checked) ? $bgImgHD : $bgImgHD75;
   renderBackground(ctxhd, canvashd, 1920, 1080);
 }
 
 // Draw the 4:3 version of the canvas
 function draw43(){
-  $activeImg = (optionSimple.checked) ? $bgImg43 : $bgImg4375;
   renderBackground(ctxorig, canvas, 1600, 1200);
 }
 
@@ -70,6 +86,7 @@ function renderBackground(context, canvasObj, w, h, type='jpg') {
 
 // Update the image based on the selected format
 function update() {
+  setActiveImg();
   ($optionHD.checked) ? drawHD() : draw43();
 }
 
@@ -93,6 +110,8 @@ function bind () {
   $optionHD.oninput = update;
   $optionSimple.oninput = update;
   $option75.oninput = update;
+  $optionGradient.oninput = update;
+  $optionTextured.oninput = update;
 }
 
 function draw(ctx, w, h) {
